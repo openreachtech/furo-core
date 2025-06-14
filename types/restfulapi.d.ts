a445e68eec7129b61fd778556dbb95ae609a5345
Stew Eucen <stew.eucen@gmail.com>
2025-06-14 18:59:23 +0900

    Define RestfulApiType in types/restfulapi.d.ts

---
+++ types/restfulapi.d.ts
@@ -0,0 +1,82 @@
+import type {
+  PayloadCtor,
+  Payload,
+  BaseRestfulApiPayloadParams as PayloadParams,
+  BaseRestfulApiPayloadFactoryParams as PayloadFactoryParams,
+  RestfulApiRequestBody as RequestVariables,
+} from '../lib/client/restfulapi/BaseRestfulApiPayload'
+
+import type {
+  CapsuleCtor,
+  Capsule,
+  BaseRestfulApiCapsuleParams as CapsuleParams,
+  BaseRestfulApiCapsuleFactoryParams as CapsuleFactoryParams,
+
+  RestfulApiResponse as Response,
+  RestfulApiResponseContent as ResponseContent,
+  RestfulApiResponseError as ResponseError,
+} from '../lib/client/restfulapi/BaseRestfulApiCapsule'
+
+import type {
+  LauncherCtor,
+  Launcher,
+  BaseRestfulApiLauncherParams as LauncherParams,
+  BaseRestfulApiLauncherFactoryParams as LauncherFactoryParams,
+
+  RestfulApiLaunchRequestArgs as LaunchRequestArgs,
+  RestfulApiLauncherHooks as LauncherHooks,
+
+  RestfulApiRequestArgs as RequestArgs,
+} from '../lib/client/restfulapi/BaseRestfulApiLauncher'
+
+type METHOD = 'GET'
+  | 'POST'
+  | 'PUT'
+  | 'PATCH'
+  | 'DELETE'
+  | 'HEAD'
+  | 'OPTIONS'
+  | 'TRACE'
+  | 'CONNECT'
+type RequestUrl = RequestInfo | URL
+
+/**
+ * Furo RESTful API types
+ */
+declare global {
+  namespace RestfulApiType {
+    export {
+      // Payload
+      PayloadCtor,
+      Payload,
+      PayloadParams,
+      PayloadFactoryParams,
+      RequestVariables,
+
+      // Capsule
+      CapsuleCtor,
+      Capsule,
+      CapsuleParams,
+      CapsuleFactoryParams,
+
+      Response,
+      ResponseContent,
+      ResponseError,
+
+      // Launcher
+      LauncherCtor,
+      Launcher,
+      LauncherParams,
+      LauncherFactoryParams,
+
+      LaunchRequestArgs,
+      LauncherHooks,
+      RequestArgs,
+
+      METHOD,
+
+      // Additional types
+      RequestUrl
+    }
+  }
+}
